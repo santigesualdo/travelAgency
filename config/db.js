63f0aba226c399c90ab8faff1a404e930b8d03fd
Santiago Gesualdo <santigesualdo@gmail.com>
2022-10-12 12:29:06 -0300

    mysql port exclude for using enviroment var

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv/config'
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER,process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: 3306,
     dialect: 'mysql',
     define: {
         timestamps: false
